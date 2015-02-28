7a25744dbeb8b2a0fc2b307818a6752daf6c9932
Alejandra Bañay <lilith.luthien@gmail.com>
2015-02-28 14:42:06 -0300

    Colectas

---
+++ colectas.php
@@ -0,0 +1,28 @@
+<?php
+	include "header.php";
+?>
+			<div class="wrap">
+				<div class="printSolo">
+				<h2 id="Viajes">Viajes de campo</h2>
+				<br />
+					[En construcción]
+				<br />
+					Es posible que se organice en viaje de colecta durante o al finalizar el encuentro.
+				</div>
+			</div>
+			
+			<div class="wrap">
+				<div class="printSolo">
+				<h2 id="Colectas">Colectas científicas en Argentina</h2>
+				<br />
+					Requisitos legales para la colecta de ejemplares y su exportación
+				<br />
+					En la República Argentina se encuentra prohibida la colecta de cualquier componente de la fauna y la flora sin la autorización de las autoridades competentes. 
+					Por ser un país de organización federal cada estado provincial posee legislación especial sobre este tema debiéndose consultar en cada caso las limitaciones existentes 
+					para la colecta y transporte de cualquier espécimen. En cada caso se deberá contar con la autorización provincial para luego poder tramitar el permiso de exportación a 
+					nivel nacional. En general se autorizan únicamente colectas en el marco de proyectos de investigación avalados por instituciones pertenecientes al sistema científico argentino.
+				</div>
+			</div>
+<?php
+	include "footer.php";
+?>
