0835279acf3dd40df245953321c059a68a272b6b
Greenderella <lilith.luthien@gmail.com>
2015-05-27 20:53:06 -0300

    Mail de colectas

--- colectas.php
+++ colectas.php
@@ -11,11 +11,16 @@
 					En el caso de colectas dentro del sistema de Parques Nacionales se deberán tramitar los permisos correspondientes en la oficina regional de la Administración de Parques Nacionales a la que pertenece el área protegida <a target="_blank" href="http://www.parquesnacionales.gob.ar/">(APN)</a>.
 				<br />
 					En todos los casos, para la exportación de ejemplares colectados en la Argentina se deberá gestionar la Solicitud de Acceso, Exportación o Importación del Acceso a los Recursos Genéticos provenientes de la diversidad biológica <a target="_blank" href="http://www.ambiente.gob.ar/?aplicacion=tramites&IdTramite=34&IdSeccion=24">(Exportación)</a> en la Dirección Nacional de Ordenamiento Ambiental y Conservación de la Biodiversidad de la Secretaria de Ambiente y Desarrollo Sustentable de la Nación <a target="_blank" href="http://www.ambiente.gov.ar/">(SADyS)</a>.
+				<br />
+				<br />
+					<h3>IMPORTANTE:</h3> 
+					A fin de tramitar los permisos de colecta, por favor comunicarse con la "DIRECCIÓN DE FLORA, FAUNA SILVESTRE Y SUELOS", cuyo director es el Ing. Zootecnista Juan Manuel Villa, mail de contacto: <a href="jvilla@tucuman.gov.ar">jvilla@tucuman.gov.ar</a>	
 				</div>
 				
 				<div class="printSolo">
 				<h2 id="Colecciones">Visitas a la colección del "Instituto y Fundación Miguel Lillo"</h2>
 					Se deberán coordinar turnos con la curadora, Licenciada Emilia Constanza Pérez, a la dirección de correo electrónico: <a href="emiliacperez@hotmail.com.ar">emiliacperez@hotmail.com.ar</a>, y las mismas se podrán realizar preferentemente en horas de la mañana. 
+				<br />
 				<br />
 					<h3>IMPORTANTE:</h3> En la semana siguiente al ELEN, hay dos días no laborables en la Universidad Nacional de Tucumán: el lunes 23 de noviembre (feriado nacional) y el jueves 26 de noviembre (día del empleado no docente de las universidades nacionales).
 				</div>
