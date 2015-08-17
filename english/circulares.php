d82200fcf2f5d3503fec842e1c438221f054458a
Greenderella <lilith.luthien@gmail.com>
2015-08-16 22:23:15 -0300

    Argggg, errores de fechas

--- english/circulares.php
+++ english/circulares.php
@@ -4,10 +4,10 @@
 			<div class="wrap">
 				<div class="printSolo">
 					<h2 id="Circulares">Announcements</h2>
-					Theres is already available the second announcement of the V ELEN.
+					There is already available the second announcement of the V ELEN.
 				<br />
 				<br />
-					<a class="descarga" href="../Descargas/2nd Announcement August 2015.pdf">1st Announcement</a>
+					<a class="descarga" href="../Descargas/2nd Announcement August 2015.pdf">2nd Announcement</a>
 				</div>
 			</div>
 
--- english/sede.php
+++ english/sede.php
@@ -23,7 +23,7 @@
 					<br />
 					<br />
 					<h3>IMPORTANT:</h3>
-					Reservations will be made with 50% of the total amount of the stay until August 31st, 2015 by transfer / deposit to the bank account or using Paypal, and the remaining 50% until October 31st, 2015.
+					Reservations will be made with 50% of the total amount of the stay until August 31st, 2015 by transfer / deposit to the bank account or using Paypal, and the remaining 50% until November 16st, 2015.
 					<br />
 					Once the transfer of the reservation is made, we request you to send the scanned proof to the e-mail address of the V ELEN (<a href="mailto:elen2015argentina@gmail.com">elen2015argentina@gmail.com</a>). In the matter you shall indicate RESERVATION_ LAST NAME. The original coupon must be presented at the Residence entrance; otherwise, the total of the stay will be charged. Indicate in the e-mail the full name which matches the payment deposit and who will you share a room with.
 					<br />
