edf36b6a29afc88170e125d1d49123c78e005dd1
Greenderella <lilith.luthien@gmail.com>
2016-03-25 22:10:20 -0300

    Galería de fotos

--- circulares.php
+++ circulares.php
@@ -7,6 +7,10 @@
 					Alentamos a todos los participantes a mandarnos sus mejores fotos para incorporar a la galería virtual del encuentro, vía mail a <a href="mailto:elen2015argentina@gmail.com">elen2015argentina@gmail.com</a>
 					o a nuestro <a target="_blank" href="https://www.facebook.com/5toELEN">Facebook <i>(facebook.com/5toELEN)</i></a>.
 				</div>
+				<div class="printSolo">
+					<div id="photos"></div>
+						<iframe src="http://embedsocial.com/facebook_album/album_photos/569635963204872" width="100%" height="1450" frameborder="0" scrolling="no" marginheight="0"  marginwidth="0"></iframe>
+				</div>
 			</div>
 
 <?php
